14541a9548413130df8f432a2e2cc59b71ab5120
skilbjo <john.skilbeck@gmail.com>
2015-09-02 01:17:39 -0700

    Fixed rc.local to correctly run apps on startup

--- rspi/duckdns/duckdns.sh
+++ rspi/duckdns/duckdns.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=skilbjo&token=103e4758-aa22-44ed-a24c-3ecf642800f4&ip=" | curl -k -o ~/Code/RSP/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=skilbjo&token=103e4758-aa22-44ed-a24c-3ecf642800f4&ip=" | curl -k -o ~/unix/duckdns/duck.log -K -
