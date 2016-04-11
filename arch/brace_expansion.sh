251364a35b0906ed8a614883f04230608a45e27b
John Skilbeck <john.skilbeck@gmail.com>
2016-04-11 13:19:34 -0700

    dat brace expansion doe

--- arch/brace_expansion.sh
+++ arch/brace_expansion.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-COMMAND="ps aux | grep node"
+UPDATE="sudo apt-get update; sudo apt-get upgrade"
+PRCOESS="ps aux | grep node"
+INSTALL="sudo apt-get install rsync"
+COMMAND=$INSTALL
 
 for i in {1..4}; do
 	echo ssh pi$i \"$COMMAND\"
